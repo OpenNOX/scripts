738e6ccb6a9a29b2cbee6cb940bb98696ac3bdbb
Christopher Kaliszewski <ckaliszewski@gmail.com>
2019-03-18 09:24:59 -0400

    Update GSuite API Checker to check auth scopes

--- GSuite-API-Checker.js
+++ GSuite-API-Checker.js
@@ -1,9 +1,14 @@
 const { google } = require('googleapis');
 
 const serviceAccountEmail = '';
-const privateKey =   '';
-const authScopes = ['https://www.googleapis.com/auth/admin.directory.user'];
+const privateKey = '';
+const authScopes = [
+  'https://www.googleapis.com/auth/admin.directory.user',
+  'https://www.googleapis.com/auth/classroom.courses'
+];
 const adminEmail = '';
+const domain = adminEmail.split('@')[1];
+const customer = '';
 
 const validate = async () => {
   console.log('Initializing client...');
@@ -19,6 +24,28 @@ const validate = async () => {
   console.log('Fetching token...');
   const { expiry_date } = await client.authorize();
   console.log(`Token fetched! Expires ${expiry_date}`);
+
+  let options = { auth: client };
+  if (customer) { options = { ...options, customer }; }
+  else { options = { ...options, domain }; }
+
+  google.admin('directory_v1').users.list(options, (error, response) => {
+    if (error) {
+      console.log('DIRECTORY ERROR');
+      console.log(error);
+    } else {
+      console.log('directory_v1 got a successful response');
+    }
+  });
+
+  google.classroom('v1').courses.list(options, (error, response) => {
+    if (error) {
+      console.log('CLASSROOM ERROR');
+      console.log(error);
+    } else {
+      console.log('classroom_v1 got  a successful response');
+    }
+  });
 };
 
 validate();
